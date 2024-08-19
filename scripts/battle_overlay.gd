49e23d5864c596f5a870bcd28e31505975e01cdd
Annikatt <anneke256@gmail.com>
2024-08-19 14:31:36 -0400

    Half the battle?

--- scripts/battle_overlay.gd
+++ scripts/battle_overlay.gd
@@ -2,10 +2,31 @@ extends Node2D
 
 @onready var viruses = $virus_dish/viruses
 
+signal attack
+signal defend
+signal heal
+signal end_turn
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 func get_virus(num):
-	return viruses.get_child(num-1)
+	return viruses.get_child(num)
+
+func get_all_viruses():
+	return viruses.get_children()
+
+
+func _on_attack_pressed():
+	emit_signal('attack')
+
+func _on_defend_pressed():
+	emit_signal('defend')
+
+func _on_heal_pressed():
+	emit_signal('heal')
+
+func _on_end_turn_pressed():
+	emit_signal('end_turn')
--- scripts/virus_dish.gd
+++ scripts/virus_dish.gd
@@ -3,8 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$viruses/virus1.id = 0
+	$viruses/virus2.id = 1
+	$viruses/virus3.id = 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
